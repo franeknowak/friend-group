acc5d8be7db0ed2310b5ae239cd67ce41af950af
Can Yurtcu <canyurtcu@gmail.com>
2023-10-19 10:22:35 +0100

    made group

--- group.py
+++ group.py
@@ -2,4 +2,37 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+    'Jill' : {
+        'job': 'biologist',
+        'age': 26,
+        'connections': {
+            'Zalika': 'Friend',
+            'John': 'Partner'
+        }
+    },
+    'Zalika' : {
+        'job': 'artist',
+        'age': 28,
+        'connections': {
+            'Jill': 'Friend'
+        }
+    },
+    'John' : {
+        'job': 'writer',
+        'age': 27,
+        'connections': {
+            'Jill': 'Partner'
+        }
+    },
+    'Nash' : {
+        'job': 'chef',
+        'age': 34,
+        'connections': {
+            'John': 'Cousin',
+            'Zalika': 'Landlord'
+        }
+    },
+}
+
+print(my_group)
