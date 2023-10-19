afda998994395bfdd21386edf120248902c853ec
Franek Nowak <franciszek.nowak.23@ucl.ac.uk>
2023-10-19 10:23:44 +0100

    Created a group of friends

--- group.py
+++ group.py
@@ -2,4 +2,27 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {'Jill': {
+                'job': 'Biologist',
+                'age': '26',
+                'connections': {
+                    'Zalika': 'friend',
+                    'John': 'partner'}},
+            'Zalika': {
+                'job': 'Artist',
+                'age': '28',
+                'connections': {
+                    'Jill': 'friend',
+                    'Nash': 'landlord'}},
+            'John': {
+                'job': 'Writer',
+                'age': '27',
+                'connections': {
+                    'Jill': 'partner',
+                    'Nash': 'cousin'}},
+            'Nash': {
+                'job': 'chef',
+                'age': '34',
+                'connections': {
+                    'John': 'cousin',
+                    'Zalika': 'landlord'}}}
