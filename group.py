3fe10a2f5cf34938b276195fcb7ab6fb43aae867
Can Yurtcu <canyurtcu@gmail.com>
2023-10-19 11:14:39 +0100

    average ages

--- group.py
+++ group.py
@@ -4,25 +4,33 @@
 
 my_group = {'Jill': {
                 'job': 'Biologist',
-                'age': '26',
+                'age': 26,
                 'connections': {
                     'Zalika': 'friend',
                     'John': 'partner'}},
             'Zalika': {
                 'job': 'Artist',
-                'age': '28',
+                'age': 28,
                 'connections': {
                     'Jill': 'friend',
                     'Nash': 'landlord'}},
             'John': {
                 'job': 'Writer',
-                'age': '27',
+                'age': 27,
                 'connections': {
                     'Jill': 'partner',
                     'Nash': 'cousin'}},
             'Nash': {
                 'job': 'chef',
-                'age': '34',
+                'age': 34,
                 'connections': {
                     'John': 'cousin',
                     'Zalika': 'landlord'}}}
+
+def average_age(dic):
+    age_list = []
+    for person in dic:
+        age_list.append(dic[person]['age'])
+    return sum(age_list)/len(age_list)
+
+print(average_age(my_group))
